fb65991baf31f3ec0f52c57cc267fb74cba058a4
Thomas Graeff <676@holbertonschool.com>
2020-04-13 00:25:52 +0000

    Initial add of the all signal single handler

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -16,5 +16,11 @@ void suspend_handler(int sig);
 int trace_signal_sender(void);
 void trace_handler(int sig, siginfo_t *siginfo, void *context);
 int pid_exist(pid_t pid);
+void all_in_one(void);
+int sigset_init(sigset_t *set, int *signals);
+int signals_block(int *signals);
+int signals_unblock(int *signals);
+int handle_pending(void (*handler)(int));
+void all_in_one_handler(int sig, siginfo_t *siginfo, void *context);
 
 #endif /* __SIGNALS_H__ */
