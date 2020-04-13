50d673b25c029700b494e36a35d436faafa39e3b
Thomas Graeff <676@holbertonschool.com>
2020-04-13 08:05:40 +0000

    Initial commit of function to handle pending signals

---
+++ 0x06-signals/104-handle_pending.c
@@ -0,0 +1,30 @@
+#include "signals.h"
+
+/**
+ * handle_pending - handler for all pending signals of current pid
+ * @handler: handler to set
+ * Return: 0 on success, -1 on error
+ */
+int handle_pending(void (*handler)(int))
+{
+	struct sigaction pending_action;
+	sigset_t pending_signals;
+	int i;
+
+	sigemptyset(&pending_signals);
+
+	pending_action.sa_handler = handler;
+
+	if (sigpending(&pending_signals) < 0)
+		return (-1);
+
+	for (i = 0; i <= SIGRTMAX; i++)
+	{
+		if (sigismember(&pending_signals, i))
+		{
+			if (sigaction(i, &pending_action, NULL) < 0)
+				return (-1);
+		}
+	}
+	return (0);
+}
