1d437915fcd32ea5d048230ec53d73248494edaa
Thomas Graeff <676@holbertonschool.com>
2020-07-31 16:56:53 -0700

    Initial commit

--- 0x09-strace/strace_4.c
+++ 0x09-strace/strace_4.c
@@ -2,47 +2,83 @@
 #include "syscalls.h"
 
 /**
- * run_tracer - runs tracer from parent
- * @pid: pid of child to be traced
- * Return: 0 on success
+ * print_syscall - prints syscall arguments
+ *
+ * @reg: struct with register information
+ * @argc: number of command line arguments
+ * @argv: command line arguments
+ * @env: environment variables
+ * @pid: pid of tracee
+ * @reg: register struct
  */
-int run_tracer(pid_t pid)
+void print_syscall(int argc, char **argv, char **env, pid_t pid, struct
+		user_regs_struct reg)
 {
-	const char *syscall;
-	int i, status, num_args = 0;
-	struct user_regs_struct reg;
+	int num_args = 0, i = 0;
 	unsigned long arg;
 
-	waitpid(pid, &status, 0);
-	ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
-	while (1)
+	ptrace(PTRACE_GETREGS, pid, 0, &reg);
+	if (strcmp(syscalls_64_g[(size_t) reg.orig_rax].name, "execve") == 0)
 	{
-		if (wait_for_syscall(pid))
-			break;
-		/* print syscall name */
-		ptrace(PTRACE_GETREGS, pid, 0, &reg);
-		syscall = (syscalls_64_g[(size_t)reg.orig_rax].name);
-		printf("%s(", syscall);
-
-		/* print syscall args */
+		printf("execve(\"%s\", [", argv[1]);
+		for (i = 1; i < argc; ++i)
+		{
+			printf("\"%s\"", argv[i]);
+			if (i < argc - 1)
+				printf(", ");
+		}
+		for (i = 0; env[i]; i++)
+			;
+		printf("], [/* %d vars */]) = 0\n", i);
+	}
+	else
+	{
 		num_args = syscalls_64_g[(size_t)reg.orig_rax].nb_params;
 		for (i = 0; i < num_args; i++)
 		{
 			arg = get_syscall_arg(reg, i);
 			if (syscalls_64_g[(size_t)reg.orig_rax].params[i] == VARARGS)
 				printf("...");
+			else if (syscalls_64_g[(size_t)reg.orig_rax].params[i] == CHAR_P)
+				printf("\"%s\"", read_string(pid, arg));
 			else
 				printf("%#lx", arg);
 			if (i < num_args - 1)
 				printf(", ");
 		}
+	}
+}
+
+/**
+ * run_tracer - runs tracer from parent
+ *
+ * @pid: pid of child to be traced
+ * @argc: number of arguments
+ * @argv: command line arguments
+ * @env: environment variables
+ * Return: 0 on success
+ */
+int run_tracer(pid_t pid, int argc, char *argv[], char **env)
+{
+	int status;
+	struct user_regs_struct reg;
+
+	waitpid(pid, &status, 0);
+	ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
+
+	while (1)
+	{
+		if (wait_for_syscall(pid) != 0)
+			break;
+		ptrace(PTRACE_GETREGS, pid, 0, &reg);
+
+		printf("%s(", syscalls_64_g[reg.orig_rax].name);
+		print_syscall(argc, argv, env, pid, reg);
 		fflush(stdout);
 		if (wait_for_syscall(pid))
 			break;
-		/* print return value of syscall */
 		ptrace(PTRACE_GETREGS, pid, 0, &reg);
-		printf(") = %#lx\n", (unsigned long)reg.rax);
-		fflush(stdout);
+		printf(") = %lx\n", (unsigned long)reg.rax);
 	}
 	return (1);
 }
@@ -87,15 +123,12 @@ int main(int argc, char **argv, char **env)
 		printf("Usage: command\n");
 		return (EXIT_FAILURE);
 	}
+
 	pid = fork();
 	if (pid == 0)
-	{
 		trace_me(argv + 1, env);
-	}
 	else if (pid > 0)
-	{
-		run_tracer(pid);
-	}
+		run_tracer(pid, argc, argv, env);
 	else
 	{
 		perror("Fork failed");
