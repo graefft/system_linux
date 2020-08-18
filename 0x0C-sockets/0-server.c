b7e38bd05016b14b9aff14d8ae6a94a838346698
Thomas Graeff <676@holbertonschool.com>
2020-08-17 23:42:13 -0700

    Initial commit of program to create socket and listen on port 12345

---
+++ 0x0C-sockets/0-server.c
@@ -0,0 +1,51 @@
+#include "sockets.h"
+
+#define port 12345
+
+/**
+ * bind_socket - assigns address with bind()
+ *
+ * @sock_id: fd of created socket
+ * Return: status or -1 on failure
+ */
+int bind_socket(int sock_id)
+{
+	int status = -1;
+	struct sockaddr_in remote;
+
+	remote.sin_family = AF_INET;
+	remote.sin_addr.s_addr = htonl(INADDR_ANY);
+	remote.sin_port = htons(port);
+	status = bind(sock_id, (struct sockaddr *)&remote, sizeof(remote));
+
+	return (status);
+}
+
+
+/**
+ * main - opens IPv4/TCP socket and listens on port 12345
+ *
+ * Return: EXIT_SUCCESS or -1
+ */
+int main(void)
+{
+	int status, sock_id;
+
+	sock_id = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_id == -1)
+	{
+		printf("Socket create failure\n");
+		return (EXIT_FAILURE);
+	}
+	status = bind_socket(sock_id);
+	if (status == -1)
+	{
+		printf("Bind didn't work\n");
+		return (EXIT_FAILURE);
+	}
+	listen(sock_id, 3);
+	printf("Server listening on port %d\n", port);
+
+	pause();
+	return (EXIT_SUCCESS);
+}
---
+++ 0x0C-sockets/sockets.h
@@ -0,0 +1,12 @@
+#ifndef __SOCKETS_H__
+#define __SOCKETS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <arpa/inet.h>
+
+#endif /* __SOCKETS_H__ */
